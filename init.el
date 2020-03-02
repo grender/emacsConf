8e377e5803555547773c88d34418d63be7e4f7bd
Alex Kopytov <grender.no8@gmail.com>
2020-03-02 23:46:19 +0300

    added go support from mobile

--- init.el
+++ init.el
@@ -37,28 +37,6 @@
   (package-install 'use-package))
 (require 'use-package)
 
-(use-package ensime
-  :ensure t
-  :pin melpa-stable)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (smartparens highlight-symbol flx-ido projectile magit ensime use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Menlo")))))
-
-(use-package monokai-theme)
-
-(load-theme 'monokai t)
-
 (use-package magit
   :commands magit-status magit-blame
   :init (setq
@@ -88,26 +66,24 @@
   :bind   (("s-f" . projectile-find-file)
            ("s-F" . projectile-grep)))
 
-(use-package projectile
-  :demand
-  :init   (setq projectile-use-git-grep t)
-  :config (projectile-global-mode t)
-  :bind   (("s-f" . projectile-find-file)
-           ("s-F" . projectile-grep)))
-
-(use-package flx-ido
-  :demand
-  :init
-  (setq
-   ido-enable-flex-matching t
-   ;; C-d to open directories
-   ;; C-f to revert to find-file
-   ido-show-dot-for-dired nil
-   ido-enable-dot-prefix t)
-  :config
-  (ido-mode 1)
-  (ido-everywhere 1)
-  (flx-ido-mode 1))
+;(use-package helm
+;  :ensure t
+;  :bind (("M-a" . helm-M-x)
+;         ("C-x C-f" . helm-find-files)
+;         ("C-x f" . helm-recentf)
+;         ("C-SPC" . helm-dabbrev)
+;         ("M-y" . helm-show-kill-ring)
+;         ("C-x b" . helm-buffers-list))
+;  :bind (:map helm-map
+;	      ("M-i" . helm-previous-line)
+;	      ("M-k" . helm-next-line)
+;	      ("M-I" . helm-previous-page)
+;	      ("M-K" . helm-next-page)
+;	      ("M-h" . helm-beginning-of-buffer)
+;	      ("M-H" . helm-end-of-buffer))
+;  :config (progn
+;	    (setq helm-buffers-fuzzy-matching t)
+;            (helm-mode 1)))
 
 (use-package highlight-symbol
   :diminish highlight-symbol-mode
@@ -145,3 +121,64 @@
 
 (add-to-list 'exec-path "/Users/grender/sbt/bin/")
 
+;(use-package monokai-theme)
+;(load-theme 'monokai t)
+
+(use-package zenburn-theme)
+
+(defvar zenburn-override-colors-alist
+  '(("zenburn-bg" . "#111111")))
+
+(load-theme 'zenburn t)
+
+
+(use-package plantuml-mode)
+(add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
+(setq plantuml-jar-path "/usr/local/Cellar/plantuml/1.2018.0/libexec/plantuml.jar")
+
+(use-package restclient)
+
+(use-package flycheck)
+(use-package multi-compile)
+(use-package go-eldoc)
+(use-package company-go)
+
+(add-hook 'before-save-hook 'gofmt-before-save)
+(setq-default gofmt-command "goimports")
+(add-hook 'go-mode-hook 'go-eldoc-setup)
+(add-hook 'go-mode-hook (lambda ()
+                            (set (make-local-variable 'company-backends) '(company-go))
+                            (company-mode)))
+(add-hook 'go-mode-hook 'yas-minor-mode)
+(add-hook 'go-mode-hook 'flycheck-mode)
+(setq multi-compile-alist '(
+    (go-mode . (
+("go-build main.go" "go build -v"
+   (locate-dominating-file buffer-file-name ".git"))
+("go-build-and-run main.go" "go build -v && echo 'build finish' && eval ./${PWD##*/}"
+   (multi-compile-locate-file-dir ".git"))))
+    ))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company-go go-eldoc multi-compile zenburn-theme use-package smartparens restclient projectile plantuml-mode org-trello monokai-theme magit highlight-symbol helm flycheck flx-ido ensime))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
+
+;; patch for working "go vet"
+;; change "go tool vet" -> "go vet"
+(let ((govet (flycheck-checker-get 'go-vet 'command)))
+  (when (equal (cadr govet) "tool")
+    (setf (cdr govet) (cddr govet))))
